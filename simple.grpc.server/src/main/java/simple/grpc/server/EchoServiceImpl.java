a346c6e1fdf77583a6dc3c19064c782ff7557387
Stephan <Stephan.Maevers@gmail.com>
2016-11-23 01:12:03 +0100

    added eureka server and grpc service

---
+++ simple.grpc.server/src/main/java/simple/grpc/server/EchoServiceImpl.java
@@ -0,0 +1,17 @@
+package simple.grpc.server;
+
+import com.example.echo.EchoOuterClass.Echo;
+import com.example.echo.EchoServiceGrpc.EchoServiceImplBase;
+
+import io.grpc.stub.StreamObserver;
+
+public class EchoServiceImpl extends EchoServiceImplBase {
+
+	@Override
+	public void echo(Echo request, StreamObserver<Echo> responseObserver) {
+		System.out.println(request);
+	    responseObserver.onNext(request);
+	    responseObserver.onCompleted();
+	}
+	
+}
---
+++ simple.grpc.server/src/main/java/simple/grpc/server/GrpcServer.java
@@ -0,0 +1,43 @@
+package simple.grpc.server;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GrpcServer {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcServer.class, args);
+	}
+	
+	public GrpcServer() {
+		Server server = ServerBuilder.forPort(9090).addService(new EchoServiceImpl()).build();
+		try {
+			server.start();
+			new Thread() {
+
+				@Override
+				public void run() {
+					try {
+						server.awaitTermination();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			}.start();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
