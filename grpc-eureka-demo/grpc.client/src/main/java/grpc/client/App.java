441a52da51d8ececca6852991e4d97d487df6c70
Stephan <Stephan.Maevers@gmail.com>
2016-11-23 14:29:23 +0100

    added new projects

---
+++ grpc-eureka-demo/grpc.client/src/main/java/grpc/client/App.java
@@ -0,0 +1,99 @@
+package grpc.client;
+
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import com.example.echo.EchoOuterClass.Echo;
+import com.example.echo.EchoServiceGrpc;
+import com.example.echo.EchoServiceGrpc.EchoServiceBlockingStub;
+
+import io.grpc.Attributes;
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.NameResolver;
+import io.grpc.NameResolver.Factory;
+import io.grpc.ResolvedServerInfo;
+import io.grpc.util.RoundRobinLoadBalancerFactory;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	public App(final DiscoveryClient client) {
+		client.getServices();
+
+		Channel channel = ManagedChannelBuilder.forTarget("EchoService")
+				.loadBalancerFactory(RoundRobinLoadBalancerFactory.getInstance()).nameResolverFactory(new Factory() {
+					@Override
+					public NameResolver newNameResolver(final URI targetUri, Attributes params) {
+						return new NameResolver() {
+							private Listener listener;
+
+							@Override
+							public void start(Listener listener) {
+								this.listener = listener;
+								refresh();
+							}
+
+							@Override
+							public void refresh() {
+								List<List<ResolvedServerInfo>> servers = new ArrayList<>();
+								for (ServiceInstance serviceInstance : client.getInstances(targetUri.toString())) {
+									System.out.println("Service Instance: " + serviceInstance.getHost() + ":"
+											+ serviceInstance.getPort());
+									servers.add(Collections.singletonList(new ResolvedServerInfo(InetSocketAddress
+											.createUnresolved(serviceInstance.getHost(), serviceInstance.getPort()),
+											Attributes.EMPTY)));
+								}
+
+								this.listener.onUpdate(servers, Attributes.EMPTY);
+							}
+
+							@Override
+							public void shutdown() {
+
+							}
+
+							@Override
+							public String getServiceAuthority() {
+								return targetUri.toString();
+							}
+						};
+					}
+
+					@Override
+					public String getDefaultScheme() {
+						return "spring";
+					}
+				}).usePlaintext(true).build();
+
+		System.out.println(channel);
+
+		EchoServiceBlockingStub stub = EchoServiceGrpc.newBlockingStub(channel);
+		for (int i = 0;; ++i) {
+			try {
+				Echo response = stub.echo(Echo.newBuilder().setMessage("Hello " + i).build());
+				System.out.println(response);
+
+				try {
+					Thread.sleep(2000L);
+				} catch (InterruptedException e) {
+				}
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+		}
+	}
+}
